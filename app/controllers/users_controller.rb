aa5121355bbcd13da6062cecfc0bf5717a033a1e
atul-koshta <atulk2018@gmail.com>
2024-03-14 19:43:50 +0530

    Refactor UsersController create_client method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,17 @@
 class UsersController < ApplicationController
   def create_client
-    puts client_params
-
     if User.exists?(username: client_params[:username])
       render json: { success: false, message: 'User with given username already exist!', data: nil }, status: :conflict
-      return
-    end
-
-    @client = User.new(client_params)
-    if @client.save!
-      render json: { success: true, message: 'Client created successfully', data: { client_id: @client.id } },
-             status: :created
     else
-      render json: { success: false, message: 'Unable to create client', data: nil }, status: :internal_server_error
+      @client = User.new(client_params)
+      begin
+        @client.save!
+        render json: { success: true, message: 'Client created successfully', data: { client_id: @client.id } },
+               status: :created
+      rescue ActiveRecord::RecordInvalid => e
+        render json: { success: false, message: 'Unable to create client', data: { errors: e.record.errors.full_messages.join(', ') } },
+               status: :internal_server_error
+      end
     end
   end
 
